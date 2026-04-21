b2c23f223573d2a1edd35f784b9dec99d0de8e7a
adithyakarikalan-hub <appouse18@gmail.com>
2026-04-21 11:59:08 +0530

    first

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const axios = require("axios");
+
 const apiKey = process.env.API_KEY;
+const apiUrl = process.env.API_URL;
 
-if (!apiKey) {
-  throw new Error("API_KEY is missing");
-}
-
-console.log("API key loaded successfully");
+axios.get(apiUrl, {
+  headers: { Authorization: `Bearer ${apiKey}` }
+})
+.then(res => console.log(res.data))
+.catch(err => console.error(err));
